16d842f70bb85656b8b9a8c38f892b729b303073
Sivadeep <166890093+Sivadeep30@users.noreply.github.com>
2025-03-21 11:57:55 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
+    docker login -u sivadeep30 -p dckr_pat_1xfxPzv4VjtG4NGw35qkNvpy4y4
+    docker tag test sivadeep30/demo1
+    docker push sivadeep30/demo1
+    # docker-compose up -d
     
